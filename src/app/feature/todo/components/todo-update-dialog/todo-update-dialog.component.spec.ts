a7e0a89dc8dc275ae4112be2379d589424a28cf9
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-04 16:42:11 +0700

    feat(dialog): initialize dialog

---
+++ src/app/feature/todo/components/todo-update-dialog/todo-update-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoUpdateDialogComponent } from './todo-update-dialog.component';
+
+describe('TodoUpdateDialogComponent', () => {
+  let component: TodoUpdateDialogComponent;
+  let fixture: ComponentFixture<TodoUpdateDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TodoUpdateDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TodoUpdateDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/feature/todo/components/todo-update-dialog/todo-update-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-update-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-update-dialog.component.html',
+  styleUrl: './todo-update-dialog.component.scss'
+})
+export class TodoUpdateDialogComponent {
+
+}
