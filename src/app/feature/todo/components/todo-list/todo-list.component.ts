861169afe697cfc3e508ed6972f9ba9987e4a6fb
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-04 16:34:32 +0700

    feat(todo-list): complete

--- src/app/feature/todo/components/todo-list/todo-list.component.ts
+++ src/app/feature/todo/components/todo-list/todo-list.component.ts
@@ -1,26 +1,29 @@
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Todo } from '@core/models';
 import { TodoService } from '@core/services/todo/todo.service';
+import { InputComponent } from '@shared/components/input/input.component';
 import { ButtonDirective } from '@shared/directives/button/button.directive';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [ButtonDirective],
+  imports: [ButtonDirective, InputComponent, ReactiveFormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
   public readonly todoService = inject(TodoService);
 
-  public onCompleteTodo(id: number) {
+  public onCompleteTodo(id: number): void {
     this.todoService.completeTodoById(id);
   }
 
-  public onUncompleteTodo(id: number) {
-    this.todoService.uncompleteTodoById(id);
+  public onDeleteTodo(id: number): void {
+    this.todoService.deleteTodoById(id);
   }
 
-  public onDeleteTodo(id: number) {
-    this.todoService.deleteTodoById(id);
+  public onUpdateTodo(todo: Todo): void {
+    this.todoService.updateTodo(todo);
   }
 }
