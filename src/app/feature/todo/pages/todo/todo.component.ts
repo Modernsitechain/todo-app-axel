ae9e7ffc368cd77a7625d58b12b9dc781307ccb4
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-04 15:15:33 +0700

    feat(todo.component): change layout

--- src/app/feature/todo/pages/todo/todo.component.ts
+++ src/app/feature/todo/pages/todo/todo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TodoCompletedListComponent } from '@feature/todo/components/todo-completed-list/todo-completed-list.component';
 import { TodoFormComponent } from '@feature/todo/components/todo-form/todo-form.component';
 import { TodoListComponent } from '@feature/todo/components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [TodoFormComponent, TodoListComponent],
+  imports: [TodoFormComponent, TodoListComponent, TodoCompletedListComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
