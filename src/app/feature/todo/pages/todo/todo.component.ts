22530f07379c3e887e29d11da377f59333a498a6
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-04 11:50:26 +0700

    feat(todo.component): completed header

--- src/app/feature/todo/pages/todo/todo.component.ts
+++ src/app/feature/todo/pages/todo/todo.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TodoFormComponent } from '@feature/todo/components/todo-form/todo-form.component';
+import { TodoListComponent } from '@feature/todo/components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [TodoFormComponent, TodoListComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
-export class TodoComponent {
-
-}
+export class TodoComponent {}
