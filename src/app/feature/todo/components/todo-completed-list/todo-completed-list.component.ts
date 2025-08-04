a8e93d4e05a3581a7fda31474ec1bfeae65d01cd
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-04 15:33:11 +0700

    feat(todo-completed-list): complete completed todo list

--- src/app/feature/todo/components/todo-completed-list/todo-completed-list.component.ts
+++ src/app/feature/todo/components/todo-completed-list/todo-completed-list.component.ts
@@ -1,14 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { TodoService } from '@core/services/todo/todo.service';
+import { ButtonDirective } from '@shared/directives/button/button.directive';
 
 @Component({
   selector: 'app-todo-completed-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonDirective],
   templateUrl: './todo-completed-list.component.html',
   styleUrl: './todo-completed-list.component.scss'
 })
 export class TodoCompletedListComponent {
-  private readonly todoService = inject(TodoService);
-  
+  public readonly todoService = inject(TodoService);
+
+  public onUncompleteTodo(id: number) {
+    this.todoService.uncompleteTodoById(id);
+  }
+
+  public onDeleteTodo(id: number) {
+    this.todoService.deleteTodoById(id);
+  }
 }
